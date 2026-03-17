e391697e003b6dd3c4ad6457c56556bf0f58e379
ignatvs <112820195+ignatvs@users.noreply.github.com>
2026-03-17 07:52:08 -0600

    Update interjecciones.js

--- interjecciones.js
+++ interjecciones.js
@@ -4,7 +4,6 @@ const interjecciones = [
   { lema: "ēheu", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡ay de mí!" },
   { lema: "euge", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡bravo!" },
   { lema: "hercle", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡por Hércules!" },
-  { lema: "salvē", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡hola!" },
   { lema: "satis", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡basta!" },
   { lema: "valē", tipo: "interjección", raiz: "-", decl: "Invariable", trad: "¡adiós!" }
 ];
